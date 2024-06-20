5b1719faf4f4f7d165a39d6614960a68419ce0ab
kagwa75 <168121892+kagwa75@users.noreply.github.com>
2024-06-20 12:09:07 +0300

    Add files via upload

---
+++ login assignment/db_config.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'kagwanja';
+$dbconfig = mysqli_connect($host,$username,$password,$database) or die("An Error occured when connecting to the database");
+?>
